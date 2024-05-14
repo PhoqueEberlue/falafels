404a9db209db10de980afb3608cd8a8a2de4494a
PhoqueEberlue <andrewmhdb@gmail.com>
2024-05-14 14:45:30 +0000

    Removed roles.cpp because we move the queues and their function in the mediators

--- src/node/roles/role.hpp
+++ src/node/roles/role.hpp
@@ -2,10 +2,8 @@
 #ifndef FALAFELS_ROLE_HPP
 #define FALAFELS_ROLE_HPP
 
-#include "../network_managers/nm.hpp"
+#include "../mediator/mediator_consumer.hpp"
 #include <memory>
-#include <optional>
-#include <queue>
 
 /**
  * Abstract class that defines a Node behaviour in a Federated Learning system.
@@ -16,32 +14,15 @@
  */
 class Role
 {
-private:
-    /** Queue of packets received through the network, that the Role can get */
-    std::shared_ptr<std::queue<std::unique_ptr<Packet>>> received_packets;
+protected:
+    unique_ptr<MediatorConsumer> mc;
 
-    /** Queue of packets that the Role wants to send via the network */
-    std::shared_ptr<std::queue<std::shared_ptr<Packet>>> to_be_sent_packets;
-
-    /** Queue of NetworkManager events */
-    std::shared_ptr<std::queue<std::unique_ptr<NetworkManager::Event>>> nm_events;
+    node_name my_node_name;
 public:
     Role(){}
-    virtual ~Role(){}
+    virtual ~Role(){} 
 
-    /** Set queues to enable communication between Role and NetworkManager */
-    void set_queues(std::shared_ptr<std::queue<std::unique_ptr<Packet>>> received, 
-                    std::shared_ptr<std::queue<std::shared_ptr<Packet>>> to_be_sent,
-                    std::shared_ptr<std::queue<std::unique_ptr<NetworkManager::Event>>> nm_events);
-
-    /** Optionally get a Packet if available */
-    std::optional<std::unique_ptr<Packet>> get_received_packet();
-
-    /** Put a packet to be sent by the NetWorkManager */
-    void put_to_be_sent_packet(Packet packet);
-
-    /** Optionally get a NMEvent if available */
-    std::optional<std::unique_ptr<NetworkManager::Event>> get_nm_event();
+    void set_mediator_consumer(std::unique_ptr<MediatorConsumer> mc) { this->mc = std::move(mc); }
 
     /* --- Functions to be implemented by the children classes --- */
     virtual void run() = 0;
