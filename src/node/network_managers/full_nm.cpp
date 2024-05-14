acd1eeabd3025e482d122865feea25f680ab402f
PhoqueEberlue <andrewmhdb@gmail.com>
2024-05-14 16:24:55 +0000

    Added FullyConnectedNetworkManager

--- src/node/network_managers/full_nm.cpp
+++ src/node/network_managers/full_nm.cpp
@@ -14,25 +14,25 @@
 #include <xbt/asserts.h>
 #include <xbt/log.h>
 
-#include "star_nm.hpp"
+#include "full_nm.hpp"
 #include "../../dot.hpp"
 #include "nm.hpp"
 
 using namespace std;
 
-XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_star_nm, "Messages specific for this example");
+XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_full_nm, "Messages specific for this example");
 
-StarNetworkManager::StarNetworkManager(NodeInfo node_info) : NetworkManager(node_info)
+FullyConnectedNetworkManager::FullyConnectedNetworkManager(NodeInfo node_info) : NetworkManager(node_info)
 {
     this->connected_nodes = new vector<NodeInfo>();
 }
 
-StarNetworkManager::~StarNetworkManager() 
+FullyConnectedNetworkManager::~FullyConnectedNetworkManager() 
 {
     delete this->connected_nodes;
 };
 
-void StarNetworkManager::handle_registration_requests()
+void FullyConnectedNetworkManager::handle_registration_requests()
 {
     xbt_assert(this->my_node_info.role == NodeRole::Aggregator);
 
@@ -41,10 +41,23 @@ void StarNetworkManager::handle_registration_requests()
         std::format("{} [label=\"{}\", color=green]", this->my_node_info.name, this->my_node_info.name)
     );
 
+    // List containing all nodes of the network that will be sent back to all nodes
+    auto nodes_to_connect = make_shared<vector<NodeInfo>>();
+    nodes_to_connect->push_back(this->my_node_info);
+
+    // First loop to add each node that made a request to the list
     for (auto request : *this->registration_requests)
     {
+        // Add to local vector that saves connected nodes
         this->connected_nodes->push_back(request.node_to_register); 
 
+        // Add to list passed as packet payload
+        nodes_to_connect->push_back(request.node_to_register);
+    }
+    
+    // Second loop to send confirmation to each nodes
+    for (auto request : *this->registration_requests)
+    {
         DOTGenerator::get_instance().add_to_cluster(
             std::format("cluster-{}", this->my_node_info.name),
             std::format("{} [label=\"{}\", color=yellow]", request.node_to_register.name, request.node_to_register.name)
@@ -55,14 +68,12 @@ void StarNetworkManager::handle_registration_requests()
             std::format("{} -> {} [color=green]", this->my_node_info.name, request.node_to_register.name)
         );
 
-        auto node_list = vector<NodeInfo>();
-        node_list.push_back(this->my_node_info);
-
         auto res_p = make_shared<Packet>(Packet(
             request.node_to_register.name, request.node_to_register.name,
-            Packet::RegistrationConfirmation(
-                make_shared<vector<NodeInfo>>(node_list)
-            )
+            Packet::RegistrationConfirmation {
+                // Here full list is sent, including the same node that we send the packet to
+                .node_list=nodes_to_connect
+            }
         ));
 
         this->send_async(res_p);
@@ -73,7 +84,7 @@ void StarNetworkManager::handle_registration_requests()
     );
 }
 
-void StarNetworkManager::send_registration_request()
+void FullyConnectedNetworkManager::send_registration_request()
 {
     // This assert is'nt true in the case of hierarchical aggregator...
     // xbt_assert(this->my_node_info.role == NodeRole::Trainer);
@@ -92,17 +103,19 @@ void StarNetworkManager::send_registration_request()
 }
 
 
-void StarNetworkManager::handle_registration_confirmation(const Packet::RegistrationConfirmation &confirmation)
+void FullyConnectedNetworkManager::handle_registration_confirmation(const Packet::RegistrationConfirmation &confirmation)
 {
     for (auto node: *confirmation.node_list)
     {
-        this->connected_nodes->push_back(node);
+        // Don't add node if its the same as the current one
+        if (node.name != this->get_my_node_name())
+            this->connected_nodes->push_back(node);
     }
 
     this->mp->put_nm_event(Mediator::NodeConnected {});
 }
 
-void StarNetworkManager::broadcast(shared_ptr<Packet> packet)
+void FullyConnectedNetworkManager::broadcast(shared_ptr<Packet> packet)
 {
     for(auto node_info : *this->connected_nodes)
     {
@@ -115,8 +128,8 @@ void StarNetworkManager::broadcast(shared_ptr<Packet> packet)
     }
 }
 
-void StarNetworkManager::route_packet(unique_ptr<Packet> packet)
+void FullyConnectedNetworkManager::route_packet(unique_ptr<Packet> packet)
 {
-    // In star_nm we route everything to the Role.
+    // In full_nm we route everything to the Role.
     this->mp->put_received_packet(std::move(packet));
 }
