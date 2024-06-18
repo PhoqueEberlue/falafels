eeb4f38e171f54004c6733eb5a8d845a1246e679
PhoqueEberlue <andrewmhdb@gmail.com>
2024-06-18 16:12:28 +0000

    Improved the aggregation function by merging tasks together when multiple local models are aggregated at the same time -> this speeds up the simulation run-time by a lot.

--- simulator/src/node/roles/aggregator/aggregator.hpp
+++ simulator/src/node/roles/aggregator/aggregator.hpp
@@ -10,6 +10,18 @@
 
 class Aggregator : public Role 
 {
+private:
+    /**
+     * Run and wait one aggregation step in parallel, sharing activities among the Host's cores.
+     */
+    void run_one_aggregation();
+
+    /**
+     * Run and wait all aggregations steps in parallel, sharing activities among the Host's cores.
+     * This method groups all tasks into a big one, which is way more efficient (in terms of simulation runtime) 
+     * than executing tasks one by one.
+     */
+    void run_multiple_aggregation(uint64_t number_local_models);
 protected:
     /** Value indicating the number of local epochs that the aggregator will ask the trainers to do. */
     uint8_t number_local_epochs = 3;
@@ -29,25 +41,13 @@ protected:
     /** Simgrid activity representing the training */
     simgrid::s4u::ActivitySet *aggregating_activities;
 
-    /** Number of aggregated models on one aggregation task */
-    uint64_t current_number_aggregated_models = 0;
-
     /** Time when the aggregator has been initialized */
     double initialization_time;
 
-    /** Boolean indicating if the Aggregator still has activities to perform */
-    bool still_has_activities = true;
-
-    /**
-     * Launch one aggregation step in parallel, sharing activities among the Host's cores.
-     */
-    void launch_one_aggregation(); 
-
     /** 
-     * Launch the aggregating activity or test if the current one has finished.
-     * @return true when aggregation is finished, otherwise false.
+     * Run and wait the aggregating activities.
      */
-    bool aggregate();
+    void aggregate();
 
     /** 
      * Sends the global model with a broadcast. It should sent it to every connected nodes of our cluster.
