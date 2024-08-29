740ac67ec300b8446693cfe0086491e3476b3abc
PhoqueEberlue <andrewmhdb@gmail.com>
2024-08-29 14:27:49 +0000

    Added power rate criteria evolution experiment and modified constants parameters so they follow the same as varying experiments

---
+++ experiments/evolution/power_rate_criteria/exec.sh
@@ -0,0 +1,9 @@
+beagle --simulation-name evolution_simulation_time_criteria \
+    --output-dir ./output \
+    --constants-path ./constants.xml \
+    --clusters-path ./cluster_no_profiles.xml \
+    --profiles-path ./profiles.xml \
+    --platform-specs ./platform_specs.xml \
+    evolution \
+    --total-number-gen 15 \
+    --evolution-criteria power_rate 
